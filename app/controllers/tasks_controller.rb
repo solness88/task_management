98ff93df5d11ccfc303f392f496bfe8ef215892e
hirokazu kobayashi <solness.stf@gmail.com>
2020-12-19 18:29:25 +0900

    削除機能まで実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,14 @@ class TasksController < ApplicationController
   end
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to tasks_path, notice: "タスクを作成しました"
-    else
+    if params[:back]
       render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "タスクを作成しました"
+      else
+        render :new
+      end
     end
   end
   def show
@@ -28,6 +32,10 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_path, notice:"タスクを削除しました"
+  end
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
   end
   private
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
 end
