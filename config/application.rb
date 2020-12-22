d655aac8b04e5fa9bb734b6ad3ac66641987fed6
hirokazu kobayashi <solness.stf@gmail.com>
2020-12-22 23:37:32 +0900

    FactoryBot実装中

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,14 @@ Bundler.require(*Rails.groups)
 module TaskManagement
   class Application < Rails::Application
     config.load_defaults 5.2
-  end
-end
-config.generators do |g|
-  g.test_framework :rspec,
-                   model_specs: true,
-                   view_specs: false,
-                   helper_specs: false,
-                   routing_specs: false,
-                   controller_specs: false,
-                   request_specs: false
+    config.generators do |g|
+      g.test_framework :rspec,
+                       model_specs: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+     end
+   end
 end
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :task do
+    task_name {'lll'}
+    detail {'ggg'}
+    deadline {'seer'}
+    status {'gdrgreg'}
+    priority {'htrhrt'}
+  end
+end
