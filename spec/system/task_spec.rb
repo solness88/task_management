0422b17071237b2041ff4a0d0eb695a383e01742
hirokazu kobayashi <solness.stf@gmail.com>
2021-01-07 23:39:07 +0900

    System Specを改訂

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -27,14 +27,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content "モスクワ"
       end
     end
-    # テスト内容を追加で記載する
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        visit tasks_path
-        task_list = all('.task_row')
-        expect(task_list[1]).to include "高雄"
-      end
-    end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
