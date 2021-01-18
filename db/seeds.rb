129515e702e480298390e0471732d20c38f06ffd
hirokazu kobayashi <solness.stf@gmail.com>
2021-01-18 23:21:09 +0900

    Fakerde

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  task_name = Faker::Games::Pokemon.name
+  detail = Faker::JapaneseMedia::DragonBall.character
+  deadline = Faker::Date.in_date_period
+  status = Faker::Military.air_force_rank
+  priority = Faker::Mountain.name
+  Task.create!(task_name: task_name,
+               detail: detail,
+               deadline: deadline,
+               status: status,
+               priority: priority
+               )
+end
