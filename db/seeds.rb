8c79fef80593892d5cba7c0425e8d931e48b6683
hirokazu kobayashi <solness.stf@gmail.com>
2021-02-08 23:19:04 +0900

    seeds.rbデータを改訂

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+User.create(name:"adminuser",
+            email:"adminuser@adminuser.com",
+            password:"12345qwert",
+            password_confirmation:"12345qwert",
+            admin:true)
+
+10.times do |n|
+  User.create(name:"user#{n}",
+              email:"user#{n}@user#{n}.com",
+              password:"12345qwert",
+              password_confirmation:"12345qwert")
+end
+
 10.times do |n|
   Task.create(task_name: "#{n}番目のタスク",
               detail:"#{n}番目のタスクの詳細",
@@ -5,11 +18,4 @@
               status:"着手中",
               priority:"高",
               user_id: n)
-end
-
-10.times do |n|
-  User.create(name:"user#{n}",
-              email:"user#{n}@user#{n}.com",
-              password:"12345qwert",
-              password_digest:"12345qwert")
 end
